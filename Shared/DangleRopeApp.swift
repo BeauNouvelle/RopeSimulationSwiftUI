1bba4ae49c93a07357ee830456c88355a5c9c3e2
Beau Nouvelle <beau@getswifty.dev>
2022-01-03 12:59:11 +1100

    Initial Commit

---
+++ Shared/DangleRopeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DangleRopeApp.swift
+//  Shared
+//
+//  Created by Beau Nouvelle on 3/1/2022.
+//
+
+import SwiftUI
+
+@main
+struct DangleRopeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
